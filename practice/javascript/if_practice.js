862bbbab215a79e252842705f910af1b335685f1
Beau Witka <bwitka@gmail.com>
2019-09-27 11:20:58 -0700

    Add all new JS practice material: logical operators, truthy/falsy, etc.

--- practice/javascript/if_practice.js
+++ practice/javascript/if_practice.js
@@ -1,4 +1,6 @@
-console.log("Hello, World!");
+/*************** CONDITIONAL STATEMENTS ***************/
+
+// IF/ELSE IF/ELSE practice:
 
 let age = Math.floor(Math.random() * 100 + 1);
 
@@ -21,3 +23,47 @@ if (age == 0) {
 } else {
   console.log("We don't know what age group you belong to...");
 }
+
+/*************** LOGICAL OPERATORS ***************/
+
+// OR operator => ||
+// Returns true if at least one of the operands is true
+if (false || false) {
+  console.log("Either was true.");
+}
+
+// AND operator => &&
+// Returns true if both operands are true
+if (true && false) {
+  console.log("Both are true.");
+}
+
+// NOT operator => !
+// Invert the value of a boolean
+
+// Combining operators:
+if ((true && true) || (false && true)) {
+  console.log("Condition evaluates to true!");
+}
+
+/*************** TRUTHY AND FALSY ***************/
+
+// falsy values:
+false;
+0;
+("");
+undefined;
+null;
+NaN;
+
+// truthy values: all values are truthy unless they are defined as falsy.
+
+/*************** END OF WEEK RECAP: what we covered ***************/
+/*
+variables
+data types
+conditional statements
+comparison operators
+logical operators
+truthy & falsy values
+*/
