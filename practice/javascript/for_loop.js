012f66b10f162f6229b9db317ad242c337f3da21
Beau Witka <bwitka@gmail.com>
2019-10-03 12:47:43 -0700

    Add for_loop.js to JS practice directory

---
+++ practice/javascript/for_loop.js
@@ -0,0 +1,19 @@
+// DO/WHILE LOOP:
+let counter = 0;
+let continueLoop = true;
+
+do {
+  console.log(`Loop number for DO/WHILE LOOP: ${counter}`);
+
+  counter += 1;
+
+  if (counter > 10) {
+    continueLoop = false;
+  }
+} while (continueLoop);
+
+// SAME PROCEDURE WITH FOR LOOP:
+
+for (let i = 0; i < 10; i++) {
+  console.log(`Loop number for FOR LOOP: ${i}`);
+}
