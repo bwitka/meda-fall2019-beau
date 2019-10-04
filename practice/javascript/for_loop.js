e642da08f2ba3e4f0d8a0bcfb6c8fa77be9b975a
Beau Witka <bwitka@gmail.com>
2019-10-04 10:47:17 -0700

    Add remaining for loop practice to for_loop.js file

--- practice/javascript/for_loop.js
+++ practice/javascript/for_loop.js
@@ -3,7 +3,7 @@ let counter = 0;
 let continueLoop = true;
 
 do {
-  console.log(`Loop number for DO/WHILE LOOP: ${counter}`);
+  // console.log(`Loop number for DO/WHILE LOOP: ${counter}`);
 
   counter += 1;
 
@@ -15,5 +15,9 @@ do {
 // SAME PROCEDURE WITH FOR LOOP:
 
 for (let i = 0; i < 10; i++) {
-  console.log(`Loop number for FOR LOOP: ${i}`);
+  // console.log(`Loop number for FOR LOOP: ${i}`);
+}
+
+for (let i = 0; i < 90; i += 2) {
+  console.log(`Hello, this is loop number ${i}.`);
 }
